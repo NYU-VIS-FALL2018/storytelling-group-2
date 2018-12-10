858de0112065440e49eb8bef812104d64fa7bbe2
Aécio Santos <aecio.solando@gmail.com>
2018-12-10 17:34:48 -0500

    Added cluster size table filtering

--- propagation-speed.js
+++ propagation-speed.js
@@ -30,48 +30,48 @@ function lineColor(d) {
   let bolsonaro = 0,
     haddad = 0;
   d.forEach(story => {
-    if (story.headline.includes("Bolsonaro")) {
+    if (story.headline.includes('Bolsonaro')) {
       bolsonaro++;
-    } else if (story.headline.includes("Haddad")) {
+    } else if (story.headline.includes('Haddad')) {
       haddad++;
     }
   });
   let color;
   if (bolsonaro > haddad) {
-    color = "stroke-blue";
+    color = 'stroke-blue';
   } else if (haddad > bolsonaro) {
-    color = "stroke-red";
+    color = 'stroke-red';
   } else {
-    color = "stroke-gray";
+    color = 'stroke-gray';
   }
-  return "line " + color;
+  return 'line ' + color;
 }
 
-function setupSlider(parentNode, onChangeCb, min, max) {
-  let sliderContainer = document.createElement("div");
-  sliderContainer.className = "cluster-size-slider";
+function setupSlider(parentNode, onChangeCb, min, max, initialMin, initialMax) {
+  let sliderContainer = document.createElement('div');
+  sliderContainer.className = 'cluster-size-slider';
 
-  let minMaxSlider = document.createElement("div");
+  let minMaxSlider = document.createElement('div');
   sliderContainer.appendChild(minMaxSlider);
 
   parentNode.parentNode.insertBefore(sliderContainer, parentNode.parentNode.childNodes[0]);
 
   noUiSlider.create(minMaxSlider, {
-    orientation: "vertical",
-    direction: "rtl",
+    orientation: 'vertical',
+    direction: 'rtl',
     tooltips: true,
     format: wNumb({
       decimals: 0
     }),
-    start: [min, max],
+    start: [initialMin, initialMax],
     connect: true,
     step: 1,
     range: {
-      min: 0,
-      max: 41
+      min: min,
+      max: max
     }
   });
-  minMaxSlider.noUiSlider.on("change", values => {
+  minMaxSlider.noUiSlider.on('change', values => {
     onChangeCb(values);
   });
 }
@@ -83,80 +83,80 @@ function renderChart(parent, data, clusters) {
   y.domain([0, d3.max(clusters, d => d.length)]);
 
   let svg = parent
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  let tooltip = parent.append("div").attr("class", "toolTip");
+  let tooltip = parent.append('div').attr('class', 'toolTip');
 
   // Add the X Axis
   svg
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
     .call(
       d3.axisBottom(x) // .ticks(6)
     );
 
   // Add the Y Axis
   svg
-    .append("g")
-    .attr("class", "y axis")
+    .append('g')
+    .attr('class', 'y axis')
     .call(d3.axisLeft(y));
 
   // Add the valueline path to the svg
   svg
-    .selectAll(".line")
+    .selectAll('.line')
     .data(clusters)
     .enter()
-    .append("path")
-    .attr("class", lineColor)
-    .style("stroke-opacity", d => opacityScale(d.length))
-    .attr("d", valueline)
-    .on("mousemove", function(d) {
+    .append('path')
+    .attr('class', lineColor)
+    .style('stroke-opacity', d => opacityScale(d.length))
+    .attr('d', valueline)
+    .on('mousemove', function(d) {
       let mousePosition = d3.mouse(parent.node());
       tooltip
-        .style("left", 60 + "px")
-        .style("top", 21 + "px")
-        .html("<b>Headline:</b> " + d[0].headline + "<br><b># Duplicates:</b> " + d.length);
+        .style('left', 60 + 'px')
+        .style('top', 21 + 'px')
+        .html('<b>Headline:</b> ' + d[0].headline + '<br><b># Duplicates:</b> ' + d.length);
     })
-    .on("mouseover", function(d) {
-      tooltip.style("display", "inline-block");
+    .on('mouseover', function(d) {
+      tooltip.style('display', 'inline-block');
     })
-    .on("mouseout", function(d) {
-      tooltip.style("display", "none");
+    .on('mouseout', function(d) {
+      tooltip.style('display', 'none');
     });
 
   function updateChart(clusters) {
-    let join = svg.selectAll(".line").data(clusters);
+    let join = svg.selectAll('.line').data(clusters);
 
     let newElements = join
       .enter()
-      .append("path")
-      .attr("class", lineColor)
-      .style("stroke-opacity", d => opacityScale(d.length))
-      .attr("d", valueline)
-      .on("mousemove", function(d) {
+      .append('path')
+      .attr('class', lineColor)
+      .style('stroke-opacity', d => opacityScale(d.length))
+      .attr('d', valueline)
+      .on('mousemove', function(d) {
         let mousePosition = d3.mouse(parent.node());
         tooltip
-          .style("left", 60 + "px")
-          .style("top", 21 + "px")
-          .html("<b>Headline:</b> " + d[0].headline + "<br><b># Duplicates:</b> " + d.length);
+          .style('left', 60 + 'px')
+          .style('top', 21 + 'px')
+          .html('<b>Headline:</b> ' + d[0].headline + '<br><b># Duplicates:</b> ' + d.length);
       })
-      .on("mouseover", function(d) {
-        tooltip.style("display", "inline-block");
+      .on('mouseover', function(d) {
+        tooltip.style('display', 'inline-block');
       })
-      .on("mouseout", function(d) {
-        tooltip.style("display", "none");
+      .on('mouseout', function(d) {
+        tooltip.style('display', 'none');
       });
 
     join
       .merge(newElements)
-      .attr("class", lineColor)
-      .style("stroke-opacity", d => opacityScale(d.length))
-      .attr("d", valueline);
+      .attr('class', lineColor)
+      .style('stroke-opacity', d => opacityScale(d.length))
+      .attr('d', valueline);
 
     join
       .exit()
@@ -168,71 +168,87 @@ function renderChart(parent, data, clusters) {
 
 function drawProgagationSpeedChart(config) {
   let { tagId, data, clusters } = config;
-  let slider = typeof config.slider === "undefined" ? true : config.slider;
-  let min = typeof config.min === "undefined" ? 0 : config.min;
-  let max = typeof config.max === "undefined" ? d3.max(clusters, d => d.length) : config.max;
+  let slider = typeof config.slider === 'undefined' ? true : config.slider;
+  let table = typeof config.slider === 'undefined' ? true : config.table;
+
+  let [min, max] = d3.extent(clusters, d => d.length);
+  let initialMin = typeof config.min === 'undefined' ? min : config.min;
+  let initialMax = typeof config.max === 'undefined' ? max : config.max;
 
   let filtered = filterStoriesByClusterSize(clusters, min, max);
   let parent = d3.select(tagId);
   let updateChart = renderChart(parent, data, filtered);
+  let updateTable = null;
+  if (table) {
+    updateTable = renderTopClustersTable('#top-clusters-table', filtered);
+  }
   onChangeFn = function(values) {
     let min = Number.parseInt(values[0]);
     let max = Number.parseInt(values[1]);
     filtered = filterStoriesByClusterSize(clusters, min, max);
     updateChart(filtered);
+    updateTable(filtered);
   };
   if (slider) {
-    setupSlider(parent.node(), onChangeFn, min, max);
+    setupSlider(parent.node(), onChangeFn, min, max, initialMin, initialMax);
   }
 }
 
+function highlightText(text) {
+  text = text.replace('Bolsonaro', '<span class="text-blue">Bolsonaro</span>');
+  text = text.replace('Haddad', '<span class="text-red">Haddad</span>');
+  text = text.replace('PT', '<span class="text-red">PT</span>');
+  text = text.replace('Lula', '<span class="text-red">Lula</span>');
+  text = text.replace('Gomes', '<span class="text-green">Gomes</span>');
+  return text;
+}
+
 function renderTopClustersTable(tagId, clusters) {
-  const columns = [{ header: "Size", style: "width:60px" }, { header: "First Headline" }];
+  const columns = [{ header: 'Size', style: 'width: 60px' }, { header: 'First Headline' }];
 
   let table = d3
     .select(tagId)
-    .append("table")
-    .attr("class", "table table-fixed");
+    .append('table')
+    .attr('class', 'table table-fixed');
 
-  let thead = table.append("thead");
-  var tbody = table.append("tbody");
+  let thead = table.append('thead').append('tr');
+  var tbody = table.append('tbody');
   // append the header row
   thead
-    .append("tr")
-    .selectAll("th")
+    .selectAll('th')
     .data(columns)
     .enter()
-    .append("th")
+    .append('th')
     .text(function(d) {
       return d.header;
     })
-    .attr("style", d => {
+    .attr('style', d => {
       if (d.style) return d.style;
     });
 
-  // create a row for each object in the data
-  var rows = tbody
-    .selectAll("tr")
-    .data(clusters)
-    .enter()
-    .append("tr");
+  function updateTable(clusters) {
+    // create a row for each object in the data
+    var rowsJoin = tbody.selectAll('tr').data(clusters);
 
-  // create a cell in each row for each column
-  var cells = rows
-    .selectAll("td")
-    .data(function(row) {
-      let text = row[0]["headline"];
-      text = text.replace("Bolsonaro", '<span class="text-blue">Bolsonaro</span>');
+    let newRows = rowsJoin.enter().append('tr');
+    rowsJoin.exit().remove();
 
-      text = text.replace("Haddad", '<span class="text-red">Haddad</span>');
-      text = text.replace("PT", '<span class="text-red">PT</span>');
-      text = text.replace("Lula", '<span class="text-red">Lula</span>');
+    var cellsJoin = rowsJoin
+      .merge(newRows)
+      .selectAll('td')
+      .data(function(cluster) {
+        return [{ value: cluster.length }, { value: highlightText(cluster[0]['headline']) }];
+      });
 
-      text = text.replace("Gomes", '<span class="text-green">Gomes</span>');
-      return [{ value: row.length }, { value: text }];
-    })
-    .enter()
-    .append("td")
+    let newCells = cellsJoin
+      .enter()
+      .append('td')
+      .html(d => d.value);
 
-    .html(d => d.value);
+    cellsJoin.merge(newCells).html(d => d.value);
+
+    cellsJoin.exit().remove();
+  }
+  updateTable(clusters);
+  return updateTable;
 }
