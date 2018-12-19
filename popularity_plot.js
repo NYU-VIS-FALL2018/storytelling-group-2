4eaa83fb3d4fe00931409b58e922d9e4e8fe1334
Aécio Santos <aecio.solando@gmail.com>
2018-12-19 16:47:17 -0500

    Format popularity_plot.js file and fix x axis

--- popularity_plot.js
+++ popularity_plot.js
@@ -1,239 +1,208 @@
 function getConfigPopularity() {
-    // set the dimensions and margins of the graph
-    let margin = { top: 20, right: 50, bottom: 50, left: 50 };
-    let height = 500 - margin.top - margin.bottom;
-    let width = 850 - margin.left - margin.right;
-    // set the ranges
-    let x = d3.scaleTime().range([0, width]);
-    let y = d3.scaleLinear().range([height, 0]);
-    return { margin, width, height, x, y};
+  // set the dimensions and margins of the graph
+  let margin = { top: 20, right: 50, bottom: 50, left: 50 };
+  let height = 500 - margin.top - margin.bottom;
+  let width = 850 - margin.left - margin.right;
+  // set the ranges
+  let x = d3.scaleTime().range([0, width]);
+  let y = d3.scaleLinear().range([height, 0]);
+  return { margin, width, height, x, y };
 }
 
-function renderPopularityPlot(tagId, dataPopu, minDate, maxDate){
-
-
-    var updateData = [];
-    var candidateData = [];
-    console.log(dataPopu);
-    for(var i = 1; i<dataPopu.columns.length; i++){
-        var candidateName = dataPopu.columns[i];
-        if(candidateName !== "lula"){
-            var values = [];
-            for(var j = 0; j<dataPopu.length; j++){
-                values.push({datetemp: dataPopu[j].day, popularity: dataPopu[j][candidateName]})
-            }
-            var objectCandidate = {name:candidateName, values:values}
-            updateData.push(objectCandidate);
-        }
-
+function renderPopularityPlot(tagId, dataPopu, minDate, maxDate) {
+    
+  var updateData = [];
+  for (var i = 1; i < dataPopu.columns.length; i++) {
+    var candidateName = dataPopu.columns[i];
+    if (candidateName !== 'lula') {
+      var values = [];
+      for (var j = 0; j < dataPopu.length; j++) {
+        values.push({ datetemp: dataPopu[j].day, popularity: dataPopu[j][candidateName] });
+      }
+      var objectCandidate = { name: candidateName, values: values };
+      updateData.push(objectCandidate);
     }
-    console.log(d3.schemeCategory10);
+  }
+  
+  data = updateData;
 
-    // for(var j = 0; j<dataPopu.length; j++){
-    //     for(var i = 1; i<dataPopu.columns.length; i++){
-    //         var candidateName = dataPopu.columns[i];
-    //         candidateData[candidateName].push({date: dataPopu[j].day, popularity: dataPopu[j][candidateName]})
-    //     }
-    // }
-    //
-    // console.log(candidateData);
+  const { width, height, margin } = getConfigPopularity();
 
-    // for(var i = 1; i<dataPopu.columns.length; i++){
-    //     var candidateName = dataPopu.columns[i];
-    //     var objectCandidate = {candidateName:[]}
-    //
-    //     for(var j = 0; j<dataPopu.length; j++){
-    //         candidateData.push()
-    //
-    //     }
-    //     var objectCandidate = {name:dataPopu.columns[i], values:[]}
-    //     updateData.push(objectCandidate);
-    // }
-    // console.log("updatedata");
-    // console.log(updateData);
-    //
-    // for(var i = 0; i<dataPopu.length; i++){
-    //     for(var j = 0; j<dataPopu.columns.length; j++) {
-    //         var candidateName = dataPopu.columns[j];
-    //
-    //
-    //         console.log(updateData[candidateName].values);
-    //         console.log(updateData[candidateName].values);
-    //         updateData[candidateName].values.push({date: dataPopu[i].day, popularity: dataPopu[i][candidateName]});
-    //     }
-    // }
-    // console.log(dataPopu[i].bolsonaro);
-    // console.log(updateData);
+  console.log(getConfigPopularity());
 
-    data =updateData;
+  var duration = 250;
 
-    const {width, height, margin} = getConfigPopularity();
+  var lineOpacity = '1';
+  var lineOpacityHover = '0.85';
+  var otherLinesOpacityHover = '0.1';
+  var lineStroke = '1.5px';
+  var lineStrokeHover = '2.5px';
 
-    console.log(getConfigPopularity());
+  var circleOpacity = '0.85';
+  var circleOpacityOnLineHover = '0.25';
+  var circleRadius = 3;
+  var circleRadiusHover = 6;
 
-    var duration = 250;
+  /* Format Data */
+  var parseDate = d3.timeParse('%Y-%m-%d');
+  data.forEach(function(d) {
+    d.values.forEach(function(d) {
+      d.date = parseDate(d.datetemp);
+      d.popularity = +d.popularity;
+    });
+  });
 
-    var lineOpacity = "0.725";
-    var lineOpacityHover = "0.85";
-    var otherLinesOpacityHover = "0.1";
-    var lineStroke = "1.5px";
-    var lineStrokeHover = "2.5px";
+  /* Scale */
+  var xScale = d3
+    .scaleTime()
+    .domain([new Date(minDate), new Date(maxDate)])
+    .range([0, width - margin.right]);
 
-    var circleOpacity = '0.85';
-    var circleOpacityOnLineHover = "0.25"
-    var circleRadius = 3;
-    var circleRadiusHover = 6;
+  var yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data[0].values, d => d.popularity)])
+    .range([height - margin.bottom, 0]);
 
-    /* Format Data */
-    var parseDate = d3.timeParse("%Y-%m-%d");
-    data.forEach(function(d) {
-        d.values.forEach(function(d) {
-            d.date = parseDate(d.datetemp);
-            d.popularity = +d.popularity;
-        });
+  var names = ['Bolsonaro', 'Haddad', 'Gomez', 'Amoêdo', 'Alckmin', 'Daciolo', 'Meirelles', 'Marina'];
+  var arrayColor = ['#1f77b4', '#d62728', '#2ca02c', '#7f7f7f', '#9467bd', '#e377c2', '#8c564b', '#ff7f0e'];
+  var color = d3.scaleOrdinal(arrayColor);
+
+  /* Add SVG */
+  var svg = d3
+    .select(tagId)
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  /* Add line into SVG */
+  var line = d3
+    .line()
+    .curve(d3.curveMonotoneX)
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.popularity));
+
+  let lines = svg.append('g').attr('class', 'lines');
+
+  lines
+    .selectAll('.line-group')
+    .data(data)
+    .enter()
+    .append('g')
+    .attr('class', 'line-group')
+    .on('mouseover', function(d, i) {
+      svg
+        .append('text')
+        .attr('class', 'title-text')
+        .style('fill', color(i))
+        .text(names[i])
+        .attr('text-anchor', 'middle')
+        .attr('x', (width - margin.right) / 2)
+        .attr('y', 20)
+        .attr('font-size', 20);
+    })
+    .on('mouseout', function(d) {
+      svg.select('.title-text').remove();
+    })
+    .append('path')
+    .attr('class', 'line')
+    .attr('d', d => line(d.values))
+    .style('stroke', (d, i) => color(i))
+    .style('opacity', lineOpacity)
+    .on('mouseover', function(d) {
+      d3.selectAll('.line').style('opacity', otherLinesOpacityHover);
+      d3.selectAll('.circle').style('opacity', circleOpacityOnLineHover);
+      d3.select(this)
+        .style('opacity', lineOpacityHover)
+        .style('stroke-width', lineStrokeHover)
+        .style('cursor', 'pointer');
+    })
+    .on('mouseout', function(d) {
+      d3.selectAll('.line').style('opacity', lineOpacity);
+      d3.selectAll('.circle').style('opacity', circleOpacity);
+      d3.select(this)
+        .style('stroke-width', lineStroke)
+        .style('cursor', 'none');
     });
 
-    /* Scale */
-    var xScale = d3.scaleTime()
-        .domain([new Date(minDate), new Date(maxDate)])
-        .range([0, width-margin.right]);
+  /* Add circles in the line */
+  lines
+    .selectAll('circle-group')
+    .data(data)
+    .enter()
+    .append('g')
+    .style('fill', (d, i) => color(i))
+    .selectAll('circle')
+    .data(d => d.values)
+    .enter()
+    .append('g')
+    .attr('class', 'circle')
+    .on('mouseover', function(d, i) {
+      d3.select(this)
+        .style('cursor', 'pointer')
+        .append('text')
+        .attr('class', 'text')
+        .text(`${String(d.date).split('00')[0]} - ${d.popularity} mentions`)
+        .attr('x', d => xScale(d.date) + 5)
+        .attr('y', d => yScale(d.popularity) - 10);
+    })
+    .on('mouseout', function(d) {
+      d3.select(this)
+        .style('cursor', 'none')
+        .transition()
+        .duration(duration)
+        .selectAll('.text')
+        .remove();
+    })
+    .append('circle')
+    .attr('cx', d => xScale(d.date))
+    .attr('cy', d => yScale(d.popularity))
+    .attr('r', circleRadius)
+    .style('opacity', 0)
+    .on('mouseover', function(d) {
+      d3.select(this)
+        .transition()
+        .duration(duration)
+        .attr('r', circleRadiusHover);
+    })
+    .on('mouseout', function(d) {
+      d3.select(this)
+        .transition()
+        .duration(duration)
+        .attr('r', circleRadius);
+    });
 
-    var yScale = d3.scaleLinear()
-        .domain([0, d3.max(data[0].values, d => d.popularity)])
-        .range([height-margin.bottom, 0]);
+  /* Add Axis into SVG */
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
 
-    var names = ["Bolsonaro", "Haddad", "Gomez", "Amoêdo", "Alckmin", "Daciolo", "Meirelles", "Marina"];
-    var arrayColor = ["#1f77b4", "#d62728", "#2ca02c", "#7f7f7f", "#9467bd", "#e377c2", "#8c564b", "#ff7f0e"];
-    var color = d3.scaleOrdinal(arrayColor);
+  svg
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
 
-    /* Add SVG */
-    var svg = d3.select(tagId).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  svg
+    .append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
 
-    /* Add line into SVG */
-    var line = d3.line()
-        .curve(d3.curveMonotoneX)
-        .x(d => xScale(d.date))
-        .y(d => yScale(d.popularity));
+  svg
+    .append('text')
+    .attr('x', 10)
+    .attr('y', 10)
+    .attr('class', 'label')
+    // .attr("transform", "    rotate(-90)")
+    // .attr('transform', 'translate(-' + (width - margin.right) / 2 + ', ' + 0 + ')')
+    .text('Number of News Mentions');
 
-    let lines = svg.append('g')
-        .attr('class', 'lines');
-
-    lines.selectAll('.line-group')
-        .data(data).enter()
-        .append('g')
-        .attr('class', 'line-group')
-        .on("mouseover", function(d, i) {
-            svg.append("text")
-                .attr("class", "title-text")
-                .style("fill", color(i))
-                .text(names[i])
-                .attr("text-anchor", "middle")
-                .attr("x", (width-margin.right)/2)
-                .attr("y", 20)
-                .attr("font-size", 20);
-        })
-        .on("mouseout", function(d) {
-            svg.select(".title-text").remove();
-        })
-        .append('path')
-        .attr('class', 'line')
-        .attr('d', d => line(d.values))
-        .style('stroke', (d, i) => color(i))
-        .style('opacity', lineOpacity)
-        .on("mouseover", function(d) {
-            d3.selectAll('.line')
-                .style('opacity', otherLinesOpacityHover);
-            d3.selectAll('.circle')
-                .style('opacity', circleOpacityOnLineHover);
-            d3.select(this)
-                .style('opacity', lineOpacityHover)
-                .style("stroke-width", lineStrokeHover)
-                .style("cursor", "pointer");
-        })
-        .on("mouseout", function(d) {
-            d3.selectAll(".line")
-                .style('opacity', lineOpacity);
-            d3.selectAll('.circle')
-                .style('opacity', circleOpacity);
-            d3.select(this)
-                .style("stroke-width", lineStroke)
-                .style("cursor", "none");
-        });
-
-
-    /* Add circles in the line */
-    lines.selectAll("circle-group")
-        .data(data).enter()
-        .append("g")
-        .style("fill", (d, i) => color(i))
-        .selectAll("circle")
-        .data(d => d.values).enter()
-        .append("g")
-        .attr("class", "circle")
-        .on("mouseover", function(d, i) {
-            d3.select(this)
-                .style("cursor", "pointer")
-                .append("text")
-                .attr("class", "text")
-                .text(`${String(d.date).split("00")[0]} -- ${d.popularity} mentions`)
-                .attr("x", d => xScale(d.date) + 5)
-                .attr("y", d => yScale(d.popularity) - 10);
-        })
-        .on("mouseout", function(d) {
-            d3.select(this)
-                .style("cursor", "none")
-                .transition()
-                .duration(duration)
-                .selectAll(".text").remove();
-        })
-        .append("circle")
-        .attr("cx", d => xScale(d.date))
-        .attr("cy", d => yScale(d.popularity))
-        .attr("r", circleRadius)
-        .style('opacity', 0)
-        .on("mouseover", function(d) {
-            d3.select(this)
-                .transition()
-                .duration(duration)
-                .attr("r", circleRadiusHover);
-        })
-        .on("mouseout", function(d) {
-            d3.select(this)
-                .transition()
-                .duration(duration)
-                .attr("r", circleRadius);
-        });
-
-
-    /* Add Axis into SVG */
-    var xAxis = d3.axisBottom(xScale).ticks(5);
-    var yAxis = d3.axisLeft(yScale).ticks(5);
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", `translate(0, ${height-margin.bottom})`)
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-    svg.append('text')
-        .attr('x', 10)
-        .attr('y', 10)
-        .attr('class', 'label')
-        .text('Number of News Mentions');
-
-    svg.append('text')
-        .attr('x', width)
-        .attr('y', height - 10)
-        .attr('text-anchor', 'end')
-        .attr('class', 'label')
-        .text('Day')
-        .attr('transform', 'translate(-' + (width-margin.right)/2 + ', ' + 0 + ')');
-
+  svg
+    .append('text')
+    .attr('x', width)
+    .attr('y', height - 10)
+    .attr('text-anchor', 'end')
+    .attr('class', 'label')
+    .text('Article Date')
+    .attr('transform', 'translate(-' + (width - margin.right) / 2 + ', ' + 0 + ')');
 }
